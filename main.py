0a446c71f30876387a6d3eb0467ef0760120fc42
Artem <artem.rmb@bk.ru>
2023-01-25 22:13:24 +0300

    Func is done

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import csv
 
 
+table = {}
+
+
 def openFile():
     fileName = str(input())
     with open(fileName, newline="") as file:
         reader = csv.DictReader(file)
+        # {A1: 1,...} ключ - значени верха и левого бока, значение - значение ячейки
         for row in reader:
-            print(row)
+            stroke = list(row.values())[0]
+            col = list(row.keys())[1:]
+            values = list(row.values())[1:]
+            for i in range(len(col)):
+                cell = col[i] + stroke
+                table[cell] = values[i]
+    print(table)
 
 
 if __name__ == "__main__":
