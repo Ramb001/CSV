e8b1b404838882b6b0ee2ef684ce177c5be91565
Artem <artem.rmb@bk.ru>
2023-01-25 21:52:20 +0300

    Add func to open csv-file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import csv
+
+
+def openFile():
+    fileName = str(input())
+    with open(fileName, newline="") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            print(row)
+
+
+if __name__ == "__main__":
+    openFile()
