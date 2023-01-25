9c7e137b6298126eebe6f7cba48114216f24fc1a
Artem <artem.rmb@bk.ru>
2023-01-25 22:50:58 +0300

    Add func for finding new values

--- main.py
+++ main.py
@@ -16,8 +16,31 @@ def openFile():
             for i in range(len(col)):
                 cell = col[i] + stroke
                 table[cell] = values[i]
+        # for row in reader:
+        #     x = row.pop(reader.fieldnames[0])
+        #     for column, value in row.items():
+        #         if column and value:
+        #             y = column
+        #             table[x,y] = value
     print(table)
+
+
+def getValues(fitem, sitem):
+    return int(table.get(fitem)) + int(table.get(sitem))
+
+
+def newValues():
+    for k, v in table.items():
+        if v[0] == '=':
+            for i in range(len(v)):
+                if v[i] == '+':
+                    index = i
+                    fitem = v[1:index]
+                    sitem = v[index+1:]
+                    table[k] = getValues(fitem, sitem)
 
 
 if __name__ == "__main__":
     openFile()
+    newValues()
+    print(table)
