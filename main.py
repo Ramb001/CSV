127f07302d2147901b4fc848190998612605cf6b
Artem <artem.rmb@bk.ru>
2023-01-27 06:07:18 +0300

    -

--- main.py
+++ main.py
@@ -2,37 +2,42 @@ import csv
 
 
 table = {}
+ops = {'+': lambda x, y: x + y,
+       '-': lambda x, y: x - y,
+       '*': lambda x, y: x * y,
+       '/': lambda x, y: x / y}
+
+
+def isInt(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
 
 
 def openFile():
     fileName = str(input())
     with open(fileName, newline="") as file:
         reader = csv.DictReader(file)
+        
         for row in reader:
             stroke = list(row.values())[0]
+            
             try:
                 int(stroke)
             except ValueError:
                 print('Value Error!')
                 quit()
+                
             col = list(row.keys())[1:]
             values = list(row.values())[1:]
+            
             for i in range(len(col)):
                 cell = col[i] + stroke
                 table[cell] = values[i]
+                
     print(table)
-
-
-def getValue(fi, si, op):
-    ops = {'+': lambda x, y: x + y,
-           '-': lambda x, y: x - y,
-           '*': lambda x, y: x * y,
-           '/': lambda x, y: x / y}
-    try:
-        return str(ops[op](float(table.get(fi)), float(table.get(si))))
-    except ZeroDivisionError:
-        print('Division by 0!')
-        quit()
 
 
 def getNames(cell):
@@ -45,14 +50,28 @@ def getNames(cell):
     return fitem, sitem, operator
 
 
+def getValue(cell, key):
+    fitem, sitem, operator = getNames(cell)
+    if table.get(fitem)[0] == '=':
+        table[fitem] = getValue(table.get(fitem), fitem)
+    try:
+        res = ops[operator](float(table.get(fitem)), float(table.get(sitem)))
+        if isInt(res): 
+            res = int(res)
+        table[key] = res
+        
+    except ZeroDivisionError:
+        print('Division by 0!')
+        quit()
+
+
 def newValues():
     for k, v in table.items():
         if v[0] == "=":
             if "+" in v or "-" in v or "*" in v or "/" in v:
-                fitem, sitem, operator = getNames(v)
-                table[k] = getValue(fitem, sitem, operator)
+                getValue(v, k)
             else:
-                print('Invalid format!')
+                print('Invalid format of cell!')
                 quit()
 
           
